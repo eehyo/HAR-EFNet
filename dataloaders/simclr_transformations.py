364634685a88f91828d224b0dfb821c00531f5bf
eehyo <eehyyo@gmail.com>
2025-06-20 22:03:56 +0900

    feat: vectorized transform

--- dataloaders/simclr_transformations.py
+++ dataloaders/simclr_transformations.py
@@ -2,242 +2,143 @@ import numpy as np
 import math
 from scipy.interpolate import CubicSpline
 import itertools
-from typing import List, Callable
+from typing import List, Callable, Tuple, Union
 
+# TODO: 9 channel version으로 변경하기
 
-def jitter_transform(X: np.ndarray, sigma: float = 0.05) -> np.ndarray:
+def noise_transform_vectorized(X: np.ndarray, sigma: float = 0.05) -> np.ndarray:
     """
-    Gaussian noise 추가
-    Args:
-        X: Input data [T, C]
-        sigma: Noise standard deviation
-    Returns:
-        Transformed data [T, C]
+    Adding random Gaussian noise with mean 0
     """
     noise = np.random.normal(loc=0, scale=sigma, size=X.shape)
     return X + noise
 
-
-def scaling_transform(X: np.ndarray, sigma: float = 0.1) -> np.ndarray:
+def scaling_transform_vectorized(X: np.ndarray, sigma: float = 0.1) -> np.ndarray:
     """
-    채널별 스케일링
-    Args:
-        X: Input data [T, C]
-        sigma: Scaling factor standard deviation
-    Returns:
-        Transformed data [T, C]
+    Scaling by a random factor
     """
-    scaling_factor = np.random.normal(loc=1.0, scale=sigma, size=(1, X.shape[1]))
+    scaling_factor = np.random.normal(loc=1.0, scale=sigma, size=(X.shape[0], 1, X.shape[2]))
     return X * scaling_factor
 
-
-def rotation_transform(X: np.ndarray) -> np.ndarray:
+def rotation_transform_vectorized(X: np.ndarray) -> np.ndarray:
     """
-    3D 회전 변환 (센서별로 적용)
-    Args:
-        X: Input data [T, C] - C는 3의 배수여야 함
-    Returns:
-        Transformed data [T, C]
+    Applying a random 3D rotation
     """
-    C = X.shape[1]
-    if C % 3 != 0:
-        raise ValueError(f"Channel dimension ({C}) must be multiple of 3 for proper sensor rotation")
-    
-    n_sensors = C // 3
-    X_rot = np.zeros_like(X)
+    axes = np.random.uniform(low=-1, high=1, size=(X.shape[0], X.shape[2]))
+    angles = np.random.uniform(low=-np.pi, high=np.pi, size=(X.shape[0]))
+    matrices = axis_angle_to_rotation_matrix_3d_vectorized(axes, angles)
 
-    for i in range(n_sensors):
-        axis = np.random.uniform(low=-1, high=1, size=3)
-        axis = axis / np.linalg.norm(axis)
-        angle = np.random.uniform(low=-np.pi, high=np.pi)
-        R = axis_angle_to_rotation_matrix(axis, angle)
+    return np.matmul(X, matrices)
 
-        vec = X[:, i*3:(i+1)*3]  # [T, 3]
-        X_rot[:, i*3:(i+1)*3] = vec @ R
-
-    return X_rot
-
-
-def axis_angle_to_rotation_matrix(axis: np.ndarray, angle: float) -> np.ndarray:
+def axis_angle_to_rotation_matrix_3d_vectorized(axes: np.ndarray, angles: np.ndarray) -> np.ndarray:
     """
-    축-각도로부터 회전행렬 생성
+    Get the rotational matrix corresponding to a rotation of (angle) radian around the axes
+
+    Reference: the Transforms3d package - transforms3d.axangles.axangle2mat
+    Formula: http://en.wikipedia.org/wiki/Rotation_matrix#Axis_and_angle
     """
-    x, y, z = axis
-    c = math.cos(angle)
-    s = math.sin(angle)
+    axes = axes / np.linalg.norm(axes, ord=2, axis=1, keepdims=True)
+    x = axes[:, 0]; y = axes[:, 1]; z = axes[:, 2]
+    c = np.cos(angles)
+    s = np.sin(angles)
     C = 1 - c
-    xs = x * s
-    ys = y * s
-    zs = z * s
-    xC = x * C
-    yC = y * C
-    zC = z * C
-    xyC = x * yC
-    yzC = y * zC
-    zxC = z * xC
-    
-    return np.array([
-        [x*xC+c, xyC-zs, zxC+ys],
-        [xyC+zs, y*yC+c, yzC-xs],
-        [zxC-ys, yzC+xs, z*zC+c]
-    ])
+
+    xs = x*s;   ys = y*s;   zs = z*s
+    xC = x*C;   yC = y*C;   zC = z*C
+    xyC = x*yC; yzC = y*zC; zxC = z*xC
+
+    m = np.array([
+        [ x*xC+c,   xyC-zs,   zxC+ys ],
+        [ xyC+zs,   y*yC+c,   yzC-xs ],
+        [ zxC-ys,   yzC+xs,   z*zC+c ]])
+    matrix_transposed = np.transpose(m, axes=(2,0,1))
+    return matrix_transposed
+
+def negate_transform_vectorized(X: np.ndarray) -> np.ndarray:
+    """
+    Inverting the signals
+    """
+    return X * -1
+
+def time_flip_transform_vectorized(X: np.ndarray) -> np.ndarray:
+    """
+    Reversing the direction of time
+    """
+    return X[:, ::-1, :]
 
 
-def negate_transform(X: np.ndarray) -> np.ndarray:
+def channel_shuffle_transform_vectorized(X: np.ndarray) -> np.ndarray:
     """
-    신호 반전
-    Args:
-        X: Input data [T, C]
-    Returns:
-        Transformed data [T, C]
+    Shuffling the different channels
+    
+    Note: it might consume a lot of memory if the number of channels is high
     """
-    return -X
+    channels = range(X.shape[2])
+    all_channel_permutations = np.array(list(itertools.permutations(channels))[1:])
+
+    random_permutation_indices = np.random.randint(len(all_channel_permutations), size=(X.shape[0]))
+    permuted_channels = all_channel_permutations[random_permutation_indices]
+    X_transformed = X[np.arange(X.shape[0])[:, np.newaxis, np.newaxis], np.arange(X.shape[1])[np.newaxis, :, np.newaxis], permuted_channels[:, np.newaxis, :]]
+    return X_transformed
+
+def time_segment_permutation_transform_improved(X: np.ndarray, num_segments: int = 4) -> np.ndarray:
+    """
+    Randomly scrambling sections of the signal
+    """
+    segment_points_permuted = np.random.choice(X.shape[1], size=(X.shape[0], num_segments))
+    segment_points = np.sort(segment_points_permuted, axis=1)
+
+    X_transformed = np.empty(shape=X.shape)
+    for i, (sample, segments) in enumerate(zip(X, segment_points)):
+        # print(sample.shape)
+        splitted = np.array(np.split(sample, np.append(segments, X.shape[1])))
+        np.random.shuffle(splitted)
+        concat = np.concatenate(splitted, axis=0)
+        X_transformed[i] = concat
+    return X_transformed
+
+def get_cubic_spline_interpolation(x_eval: np.ndarray, x_data: np.ndarray, y_data: np.ndarray) -> np.ndarray:
+    """
+    Get values for the cubic spline interpolation
+    """
+    cubic_spline = CubicSpline(x_data, y_data)
+    return cubic_spline(x_eval)
 
 
-def horizontal_flip_transform(X: np.ndarray) -> np.ndarray:
+def time_warp_transform_improved(X: np.ndarray, sigma: float = 0.2, num_knots: int = 4) -> np.ndarray:
     """
-    시간 축 반전
-    Args:
-        X: Input data [T, C]
-    Returns:
-        Transformed data [T, C]
+    Stretching and warping the time-series
     """
-    return np.flip(X, axis=0)
+    time_stamps = np.arange(X.shape[1])
+    knot_xs = np.arange(0, num_knots + 2, dtype=float) * (X.shape[1] - 1) / (num_knots + 1)
+    spline_ys = np.random.normal(loc=1.0, scale=sigma, size=(X.shape[0] * X.shape[2], num_knots + 2))
 
+    spline_values = np.array([get_cubic_spline_interpolation(time_stamps, knot_xs, spline_ys_individual) for spline_ys_individual in spline_ys])
 
-def channel_shuffle_transform(X: np.ndarray) -> np.ndarray:
-    """
-    채널 순서 셔플
-    Args:
-        X: Input data [T, C]
-    Returns:
-        Transformed data [T, C]
-    """
-    C = X.shape[1]
-    perm = np.random.permutation(C)
-    return X[:, perm]
+    cumulative_sum = np.cumsum(spline_values, axis=1)
+    distorted_time_stamps_all = cumulative_sum / cumulative_sum[:, -1][:, np.newaxis] * (X.shape[1] - 1)
 
+    X_transformed = np.empty(shape=X.shape)
+    for i, distorted_time_stamps in enumerate(distorted_time_stamps_all):
+        X_transformed[i // X.shape[2], :, i % X.shape[2]] = np.interp(time_stamps, distorted_time_stamps, X[i // X.shape[2], :, i % X.shape[2]])
+    return X_transformed
 
-def permutation_transform(X: np.ndarray, nPerm: int = 4, minSegLength: int = 10) -> np.ndarray:
+def time_warp_transform_low_cost(X: np.ndarray, sigma: float = 0.2, num_knots: int = 4, num_splines: int = 150) -> np.ndarray:
     """
-    시간 구간 순열 변환
-    Args:
-        X: Input data [T, C]
-        nPerm: Number of segments
-        minSegLength: Minimum segment length
-    Returns:
-        Transformed data [T, C]
+    Stretching and warping the time-series (low cost)
     """
-    X_new = np.zeros_like(X)
-    while True:
-        segs = np.zeros(nPerm + 1, dtype=int)
-        segs[1:-1] = np.sort(np.random.randint(minSegLength, X.shape[0] - minSegLength, nPerm - 1))
-        segs[-1] = X.shape[0]
-        if np.min(segs[1:] - segs[:-1]) > minSegLength:
-            break
-    
-    idx = np.random.permutation(nPerm)
-    pp = 0
-    for ii in range(nPerm):
-        x_temp = X[segs[idx[ii]]:segs[idx[ii] + 1], :]
-        X_new[pp:pp + len(x_temp), :] = x_temp
-        pp += len(x_temp)
-    
-    return X_new
+    time_stamps = np.arange(X.shape[1])
+    knot_xs = np.arange(0, num_knots + 2, dtype=float) * (X.shape[1] - 1) / (num_knots + 1)
+    spline_ys = np.random.normal(loc=1.0, scale=sigma, size=(num_splines, num_knots + 2))
 
+    spline_values = np.array([get_cubic_spline_interpolation(time_stamps, knot_xs, spline_ys_individual) for spline_ys_individual in spline_ys])
 
-def time_warp_transform(X: np.ndarray, sigma: float = 0.2, knot: int = 4) -> np.ndarray:
-    """
-    시간 워핑 변환
-    Args:
-        X: Input data [T, C]
-        sigma: Warping strength
-        knot: Number of knots for cubic spline
-    Returns:
-        Transformed data [T, C]
-    """
-    xx = np.linspace(0, X.shape[0] - 1, num=knot + 2)
-    yy = np.random.normal(loc=1.0, scale=sigma, size=(knot + 2, X.shape[1]))
-    x_range = np.arange(X.shape[0])
-    
-    tt_cum = np.zeros_like(X)
-    for i in range(X.shape[1]):
-        cs = CubicSpline(xx, yy[:, i])
-        warped = cs(x_range)
-        tt_cum[:, i] = np.cumsum(warped)
-        t_scale = (X.shape[0] - 1) / tt_cum[-1, i]
-        tt_cum[:, i] *= t_scale
-    
-    X_new = np.zeros_like(X)
-    for i in range(X.shape[1]):
-        X_new[:, i] = np.interp(x_range, tt_cum[:, i], X[:, i])
-    
-    return X_new
+    cumulative_sum = np.cumsum(spline_values, axis=1)
+    distorted_time_stamps_all = cumulative_sum / cumulative_sum[:, -1][:, np.newaxis] * (X.shape[1] - 1)
 
+    random_indices = np.random.randint(num_splines, size=(X.shape[0] * X.shape[2]))
 
-def get_simclr_transforms() -> List[Callable]:
-    """
-    SimCLR을 위한 변환 함수 리스트 반환
-    
-    Returns:
-        List of transformation functions
-    """
-    transforms = [
-        lambda x: jitter_transform(x, sigma=0.05),
-        lambda x: scaling_transform(x, sigma=0.1),
-        rotation_transform,
-        negate_transform,
-        horizontal_flip_transform,
-        lambda x: permutation_transform(x, nPerm=4, minSegLength=10),
-        lambda x: time_warp_transform(x, sigma=0.2, knot=4),
-        channel_shuffle_transform
-    ]
-    
-    return transforms
-
-
-def apply_random_transform_pair(X: np.ndarray) -> tuple:
-    """
-    두 개의 서로 다른 랜덤 변환을 적용하여 positive pair 생성
-    
-    Args:
-        X: Input data [T, C]
-        
-    Returns:
-        Tuple of (transformed_x1, transformed_x2)
-    """
-    transforms = get_simclr_transforms()
-    
-    # 두 개의 서로 다른 변환을 랜덤하게 선택
-    selected_transforms = np.random.choice(len(transforms), size=2, replace=False)
-    
-    transform1 = transforms[selected_transforms[0]]
-    transform2 = transforms[selected_transforms[1]]
-    
-    x1 = transform1(X.copy())
-    x2 = transform2(X.copy())
-    
-    return x1, x2
-
-
-def apply_transform_combinations(X: np.ndarray) -> List[tuple]:
-    """
-    모든 변환 조합으로 positive pairs 생성 (validation용)
-    
-    Args:
-        X: Input data [T, C]
-        
-    Returns:
-        List of transformation pairs
-    """
-    transforms = get_simclr_transforms()
-    pairs = []
-    
-    # 모든 변환 조합 생성
-    for i in range(len(transforms)):
-        for j in range(i+1, len(transforms)):
-            x1 = transforms[i](X.copy())
-            x2 = transforms[j](X.copy())
-            pairs.append((x1, x2))
-    
-    return pairs
+    X_transformed = np.empty(shape=X.shape)
+    for i, random_index in enumerate(random_indices):
+        X_transformed[i // X.shape[2], :, i % X.shape[2]] = np.interp(time_stamps, distorted_time_stamps_all[random_index], X[i // X.shape[2], :, i % X.shape[2]])
+    return X_transformed
